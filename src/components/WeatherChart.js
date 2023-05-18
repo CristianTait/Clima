2a3faea27c158c60651e345893ad6c5ca9f0cdfc
Cristian Tait <90878440+CristianTait@users.noreply.github.com>
2023-05-17 22:01:20 -0300

    Update WeatherChart.js

--- src/components/WeatherChart.js
+++ src/components/WeatherChart.js
@@ -6,7 +6,7 @@ function WeatherDisplay({ location }) {
   const [weatherData, setWeatherData] = useState(null);
 
   useEffect(() => {
-    const apiKey = '143eebf1a5d27a3d640a1481e41a204a'; 
+    const apiKey = 'APY_KEY';
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`;
     const fetchWeatherData = async () => {
       try {
